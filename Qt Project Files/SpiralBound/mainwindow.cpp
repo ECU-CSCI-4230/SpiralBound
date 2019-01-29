e2fd2d95191993b23a3c639dabe1d2b5f4f9f9b5
ellisn15 <ellisn15@students.ecu.edu>
2019-01-29 15:08:58 -0500

    Update mainwindow.cpp
    Added documentation

--- Qt Project Files/SpiralBound/mainwindow.cpp
+++ Qt Project Files/SpiralBound/mainwindow.cpp
@@ -13,8 +13,9 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
-
+// Author: Nicholas Ellis
+// Init date: 01-29-2019
+// Last Updated: 01-29-2019
 void MainWindow::on_pushButton_3_clicked()
 {
     // Print to Console
