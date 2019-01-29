4aedddf058a002cada9844586720af59ce5c1b27
ellisn15 <ellisn15@students.ecu.edu>
2019-01-29 16:54:55 -0500

    Action Methods

--- Qt Project Files/SpiralBound/mainwindow.cpp
+++ Qt Project Files/SpiralBound/mainwindow.cpp
@@ -21,7 +21,82 @@ void MainWindow::on_action_new_triggered()
     qInfo("action_new triggered");
 }
 
-void MainWindow::on_actionQuit_triggered()
+void MainWindow::on_action_openRecent_triggered()
+{
+
+}
+
+void MainWindow::on_action_saveAs_triggered()
+{
+
+}
+
+void MainWindow::on_action_bold_triggered()
+{
+
+}
+
+void MainWindow::on_action_italic_triggered()
+{
+
+}
+
+void MainWindow::on_action_underline_triggered()
+{
+
+}
+
+void MainWindow::on_action_comment_triggered()
+{
+
+}
+
+void MainWindow::on_action_strikethrough_triggered()
+{
+
+}
+
+void MainWindow::on_action_indent_triggered()
+{
+
+}
+
+void MainWindow::on_action_unindent_triggered()
+{
+
+}
+
+void MainWindow::on_action_bulletedList_triggered()
+{
+
+}
+
+void MainWindow::on_action_numberedList_triggered()
+{
+
+}
+
+void MainWindow::on_action_taskList_triggered()
+{
+
+}
+
+void MainWindow::on_action_Preferences_triggered()
+{
+
+}
+
+void MainWindow::on_action_printPreview_triggered()
+{
+
+}
+
+void MainWindow::on_action_Export_triggered()
+{
+
+}
+
+void MainWindow::on_action_Quit_triggered()
 {
     close();
 }
