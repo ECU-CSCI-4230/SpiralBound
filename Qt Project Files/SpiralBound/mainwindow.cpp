5b478691fe870e768f5aa5eb8c631df3280e467a
ellisn15 <ellisn15@students.ecu.edu>
2019-01-29 17:15:05 -0500

    Added Method Headers

--- Qt Project Files/SpiralBound/mainwindow.cpp
+++ Qt Project Files/SpiralBound/mainwindow.cpp
@@ -25,124 +25,193 @@ MainWindow::~MainWindow()
 
 // Actions
 
-
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_open_triggered()
 {
 
 }
 
-
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_test_triggered()
 {
 
 }
 
-
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_save_triggered()
 {
 
 }
 
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_about_triggered()
 {
 
 }
 
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_aboutQt_triggered()
 {
 
 }
 
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_crashCourse_triggered()
 {
 
 }
 
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_print_triggered()
 {
 
 }
 
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_new_triggered()
 {
 
 }
 
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_openRecent_triggered()
 {
 
 }
 
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_saveAs_triggered()
 {
 
 }
 
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_bold_triggered()
 {
 
 }
 
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_italic_triggered()
 {
 
 }
 
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_underline_triggered()
 {
 
 }
 
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_comment_triggered()
 {
 
 }
 
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_strikethrough_triggered()
 {
 
 }
 
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_indent_triggered()
 {
 
 }
 
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_unindent_triggered()
 {
 
 }
 
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_bulletedList_triggered()
 {
 
 }
 
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_numberedList_triggered()
 {
 
 }
 
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_taskList_triggered()
 {
 
 }
 
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_preferences_triggered()
 {
 
 }
 
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_printPreview_triggered()
 {
 
 }
 
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_export_triggered()
 {
 
 }
 
+// Author:
+// Init date:
+// Last Updated:
 void MainWindow::on_action_quit_triggered()
 {
 
--- Qt Project Files/SpiralBound/mainwindow.h
+++ Qt Project Files/SpiralBound/mainwindow.h
@@ -16,11 +16,30 @@ public:
     ~MainWindow();
 
 private slots:
-
-
     void on_action_open_triggered();
-
     void on_action_test_triggered();
+    void on_action_save_triggered();
+    void on_action_about_triggered();
+    void on_action_aboutQt_triggered();
+    void on_action_crashCourse_triggered();
+    void on_action_print_triggered();
+    void on_action_new_triggered();
+    void on_action_openRecent_triggered();
+    void on_action_saveAs_triggered();
+    void on_action_bold_triggered();
+    void on_action_italic_triggered();
+    void on_action_underline_triggered();
+    void on_action_comment_triggered();
+    void on_action_strikethrough_triggered();
+    void on_action_indent_triggered();
+    void on_action_unindent_triggered();
+    void on_action_bulletedList_triggered();
+    void on_action_numberedList_triggered();
+    void on_action_taskList_triggered();
+    void on_action_preferences_triggered();
+    void on_action_printPreview_triggered();
+    void on_action_export_triggered();
+    void on_action_quit_triggered();
 
 private:
     Ui::MainWindow *ui;
