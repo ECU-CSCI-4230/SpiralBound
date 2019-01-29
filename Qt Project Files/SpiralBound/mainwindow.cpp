12d7784a9d574c25fb5d7d3609ae031fb4e1fa96
Cam <abellc16@students.ecu.edu>
2019-01-29 14:23:06 -0500

    Update mainwindow.cpp
    Just a test to make sure my Github is working

--- Qt Project Files/SpiralBound/mainwindow.cpp
+++ Qt Project Files/SpiralBound/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+// This is a test using comments
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
