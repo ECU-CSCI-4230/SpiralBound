a5b6a091f8e75b700dfcdd1d1ff4c0c0bdf7139c
ellisn15 <ellisn15@students.ecu.edu>
2019-02-05 13:39:50 -0500

    Updated aboutspiralbound Dialog Box
    Fixed layout

--- Qt Project Files/build-SpiralBound-Desktop_Qt_5_12_0_clang_64bit-Debug/ui_aboutspiralbound.h
+++ Qt Project Files/build-SpiralBound-Desktop_Qt_5_12_0_clang_64bit-Debug/ui_aboutspiralbound.h
@@ -14,7 +14,6 @@
 #include <QtWidgets/QDialog>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QPushButton>
-#include <QtWidgets/QTextBrowser>
 
 QT_BEGIN_NAMESPACE
 
@@ -22,21 +21,18 @@ class Ui_AboutSpiralBound
 {
 public:
     QPushButton *pushButton_ok;
-    QTextBrowser *textBrowser;
     QLabel *label_logo;
+    QLabel *label;
 
     void setupUi(QDialog *AboutSpiralBound)
     {
         if (AboutSpiralBound->objectName().isEmpty())
             AboutSpiralBound->setObjectName(QString::fromUtf8("AboutSpiralBound"));
-        AboutSpiralBound->resize(402, 259);
+        AboutSpiralBound->resize(350, 200);
         AboutSpiralBound->setModal(false);
         pushButton_ok = new QPushButton(AboutSpiralBound);
         pushButton_ok->setObjectName(QString::fromUtf8("pushButton_ok"));
-        pushButton_ok->setGeometry(QRect(190, 230, 91, 24));
-        textBrowser = new QTextBrowser(AboutSpiralBound);
-        textBrowser->setObjectName(QString::fromUtf8("textBrowser"));
-        textBrowser->setGeometry(QRect(110, 20, 251, 192));
+        pushButton_ok->setGeometry(QRect(180, 160, 91, 24));
         label_logo = new QLabel(AboutSpiralBound);
         label_logo->setObjectName(QString::fromUtf8("label_logo"));
         label_logo->setGeometry(QRect(20, 20, 71, 61));
@@ -44,9 +40,15 @@ public:
         label_logo->setPixmap(QPixmap(QString::fromUtf8(":/SpiralBound/images/icon.png")));
         label_logo->setScaledContents(true);
         label_logo->setAlignment(Qt::AlignCenter);
-        textBrowser->raise();
-        pushButton_ok->raise();
-        label_logo->raise();
+        label = new QLabel(AboutSpiralBound);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(100, 20, 251, 131));
+        QFont font;
+        font.setPointSize(24);
+        label->setFont(font);
+        label->setTextFormat(Qt::RichText);
+        label->setAlignment(Qt::AlignLeading|Qt::AlignLeft|Qt::AlignTop);
+        label->setWordWrap(true);
 
         retranslateUi(AboutSpiralBound);
 
@@ -57,16 +59,8 @@ public:
     {
         AboutSpiralBound->setWindowTitle(QApplication::translate("AboutSpiralBound", "About SpiralBound", nullptr));
         pushButton_ok->setText(QApplication::translate("AboutSpiralBound", "Ok", nullptr));
-        textBrowser->setHtml(QApplication::translate("AboutSpiralBound", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:'.SF NS Text'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:24pt;\">SpiralBound </span></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Is an open source digital notebook and planner. It combines aspects from note taking and calendar applications. On top of this it incorporates study tools to aid you in your learning process.</p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
-"<p style=\"-qt-paragra"
-                        "ph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", nullptr));
         label_logo->setText(QString());
+        label->setText(QApplication::translate("AboutSpiralBound", "<html><head/><body><p>About Spiral Bound</p><p><span style=\" font-size:12pt;\">SpiralBound is an open source digital notebook and planner. It combines aspects from note taking and calendar applications. On top of this it incorporates study tools to aid you in your learning process.</span></p></body></html>", nullptr));
     } // retranslateUi
 
 };
