1c32c5f1a94c1316d8ff688b577c768f51942a2b
ellisn15 <ellisn15@students.ecu.edu>
2019-02-28 13:15:49 -0500

    Update moc_mainwindow.cpp
    Merge from Calendar branch

--- Qt Project Files/build-SpiralBound-Desktop_Qt_5_12_0_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
+++ Qt Project Files/build-SpiralBound-Desktop_Qt_5_12_0_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -21,8 +21,6 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[26];
-    char stringdata0[693];
     QByteArrayData data[36];
     char stringdata0[900];
 };
@@ -34,31 +32,6 @@ struct qt_meta_stringdata_MainWindow_t {
 static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
     {
 QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
-QT_MOC_LITERAL(1, 11, 24), // "on_action_open_triggered"
-QT_MOC_LITERAL(2, 36, 0), // ""
-QT_MOC_LITERAL(3, 37, 24), // "on_action_test_triggered"
-QT_MOC_LITERAL(4, 62, 24), // "on_action_save_triggered"
-QT_MOC_LITERAL(5, 87, 25), // "on_action_about_triggered"
-QT_MOC_LITERAL(6, 113, 27), // "on_action_aboutQt_triggered"
-QT_MOC_LITERAL(7, 141, 31), // "on_action_crashCourse_triggered"
-QT_MOC_LITERAL(8, 173, 25), // "on_action_print_triggered"
-QT_MOC_LITERAL(9, 199, 23), // "on_action_new_triggered"
-QT_MOC_LITERAL(10, 223, 30), // "on_action_openRecent_triggered"
-QT_MOC_LITERAL(11, 254, 26), // "on_action_saveAs_triggered"
-QT_MOC_LITERAL(12, 281, 24), // "on_action_bold_triggered"
-QT_MOC_LITERAL(13, 306, 26), // "on_action_italic_triggered"
-QT_MOC_LITERAL(14, 333, 29), // "on_action_underline_triggered"
-QT_MOC_LITERAL(15, 363, 27), // "on_action_comment_triggered"
-QT_MOC_LITERAL(16, 391, 33), // "on_action_strikethrough_trigg..."
-QT_MOC_LITERAL(17, 425, 26), // "on_action_indent_triggered"
-QT_MOC_LITERAL(18, 452, 28), // "on_action_unindent_triggered"
-QT_MOC_LITERAL(19, 481, 32), // "on_action_bulletedList_triggered"
-QT_MOC_LITERAL(20, 514, 32), // "on_action_numberedList_triggered"
-QT_MOC_LITERAL(21, 547, 28), // "on_action_taskList_triggered"
-QT_MOC_LITERAL(22, 576, 31), // "on_action_preferences_triggered"
-QT_MOC_LITERAL(23, 608, 32), // "on_action_printPreview_triggered"
-QT_MOC_LITERAL(24, 641, 26), // "on_action_export_triggered"
-QT_MOC_LITERAL(25, 668, 24) // "on_action_quit_triggered"
 QT_MOC_LITERAL(1, 11, 12), // "sendEditData"
 QT_MOC_LITERAL(2, 24, 0), // ""
 QT_MOC_LITERAL(3, 25, 24), // "on_action_open_triggered"
@@ -96,8 +69,6 @@ QT_MOC_LITERAL(34, 889, 3), // "row"
 QT_MOC_LITERAL(35, 893, 6) // "column"
 
     },
-    "MainWindow\0on_action_open_triggered\0"
-    "\0on_action_test_triggered\0"
     "MainWindow\0sendEditData\0\0"
     "on_action_open_triggered\0"
     "on_action_test_triggered\0"
@@ -122,7 +93,6 @@ QT_MOC_LITERAL(35, 893, 6) // "column"
     "on_action_preferences_triggered\0"
     "on_action_printPreview_triggered\0"
     "on_action_export_triggered\0"
-    "on_action_quit_triggered"
     "on_action_quit_triggered\0"
     "on_pushButton_addEvent_clicked\0"
     "on_pushButton_deleteEvent_clicked\0"
@@ -140,43 +110,17 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-      24,   14, // methods
       32,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       0,       // signalCount
        1,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
        1,    3,  174,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,  134,    2, 0x08 /* Private */,
-       3,    0,  135,    2, 0x08 /* Private */,
-       4,    0,  136,    2, 0x08 /* Private */,
-       5,    0,  137,    2, 0x08 /* Private */,
-       6,    0,  138,    2, 0x08 /* Private */,
-       7,    0,  139,    2, 0x08 /* Private */,
-       8,    0,  140,    2, 0x08 /* Private */,
-       9,    0,  141,    2, 0x08 /* Private */,
-      10,    0,  142,    2, 0x08 /* Private */,
-      11,    0,  143,    2, 0x08 /* Private */,
-      12,    0,  144,    2, 0x08 /* Private */,
-      13,    0,  145,    2, 0x08 /* Private */,
-      14,    0,  146,    2, 0x08 /* Private */,
-      15,    0,  147,    2, 0x08 /* Private */,
-      16,    0,  148,    2, 0x08 /* Private */,
-      17,    0,  149,    2, 0x08 /* Private */,
-      18,    0,  150,    2, 0x08 /* Private */,
-      19,    0,  151,    2, 0x08 /* Private */,
-      20,    0,  152,    2, 0x08 /* Private */,
-      21,    0,  153,    2, 0x08 /* Private */,
-      22,    0,  154,    2, 0x08 /* Private */,
-      23,    0,  155,    2, 0x08 /* Private */,
-      24,    0,  156,    2, 0x08 /* Private */,
-      25,    0,  157,    2, 0x08 /* Private */,
        3,    0,  181,    2, 0x08 /* Private */,
        4,    0,  182,    2, 0x08 /* Private */,
        5,    0,  183,    2, 0x08 /* Private */,
@@ -254,30 +198,6 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         MainWindow *_t = static_cast<MainWindow *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: _t->on_action_open_triggered(); break;
-        case 1: _t->on_action_test_triggered(); break;
-        case 2: _t->on_action_save_triggered(); break;
-        case 3: _t->on_action_about_triggered(); break;
-        case 4: _t->on_action_aboutQt_triggered(); break;
-        case 5: _t->on_action_crashCourse_triggered(); break;
-        case 6: _t->on_action_print_triggered(); break;
-        case 7: _t->on_action_new_triggered(); break;
-        case 8: _t->on_action_openRecent_triggered(); break;
-        case 9: _t->on_action_saveAs_triggered(); break;
-        case 10: _t->on_action_bold_triggered(); break;
-        case 11: _t->on_action_italic_triggered(); break;
-        case 12: _t->on_action_underline_triggered(); break;
-        case 13: _t->on_action_comment_triggered(); break;
-        case 14: _t->on_action_strikethrough_triggered(); break;
-        case 15: _t->on_action_indent_triggered(); break;
-        case 16: _t->on_action_unindent_triggered(); break;
-        case 17: _t->on_action_bulletedList_triggered(); break;
-        case 18: _t->on_action_numberedList_triggered(); break;
-        case 19: _t->on_action_taskList_triggered(); break;
-        case 20: _t->on_action_preferences_triggered(); break;
-        case 21: _t->on_action_printPreview_triggered(); break;
-        case 22: _t->on_action_export_triggered(); break;
-        case 23: _t->on_action_quit_triggered(); break;
         case 0: _t->sendEditData((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2])),(*reinterpret_cast< QString(*)>(_a[3]))); break;
         case 1: _t->on_action_open_triggered(); break;
         case 2: _t->on_action_test_triggered(); break;
@@ -322,7 +242,6 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
             }
         }
     }
-    Q_UNUSED(_a);
 }
 
 QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
@@ -354,16 +273,12 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 24)
         if (_id < 32)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 24;
         _id -= 32;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 24)
         if (_id < 32)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 24;
         _id -= 32;
     }
     return _id;
