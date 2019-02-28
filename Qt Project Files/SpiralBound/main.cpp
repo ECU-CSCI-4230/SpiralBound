dd2d279acd30457a7618316d240c01431dbff4be
ellisn15 <ellisn15@students.ecu.edu>
2019-02-28 13:16:19 -0500

    Update main.cpp
    Merge from Calendar branch

--- Qt Project Files/SpiralBound/main.cpp
+++ Qt Project Files/SpiralBound/main.cpp
@@ -3,6 +3,9 @@
 
 int main(int argc, char *argv[])
 {
+    // Sets fonts, windows, etc. to a consistent size across platforms
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
