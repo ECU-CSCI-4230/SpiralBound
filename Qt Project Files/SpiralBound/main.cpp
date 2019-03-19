3f1e8dd46ac51a96868f114880a45ecf82435401
Cam <abellc16@students.ecu.edu>
2019-03-19 15:34:52 -0400

    Functionality for a Splash Screen
    Added the picture we are going to use, and the necessary code.

--- Qt Project Files/SpiralBound/main.cpp
+++ Qt Project Files/SpiralBound/main.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSplashScreen>
+#include <QTimer>
 
 int main(int argc, char *argv[])
 {
@@ -7,7 +9,16 @@ int main(int argc, char *argv[])
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QApplication a(argc, argv);
+
+    // Code for splash screen
+    QSplashScreen *splash = new QSplashScreen;
+    splash->setPixmap(QPixmap(":/SpiralBound/images/SplashScreen.png"));
+    splash->show();
+
     MainWindow w;
-    w.show();
+
+    QTimer::singleShot(5000, splash, SLOT(close()));
+    QTimer::singleShot(5000, &w, SLOT(show()));
+    // w.show();
     return a.exec();
 }
