36f379193e1aaaefa4b3193af6a9f0b91b57c676
Cam <abellc16@students.ecu.edu>
2019-06-13 18:59:29 -0400

    Fix out of bounds issue while studying decks & added comments to studycard.cpp
    Closes: #1
    - Disallowed users from going past the end of a list of cards
    - Disallowed users from going beyond the beginning of a list of cards
    - Added relevant comments to studycard.cpp

--- Qt Project Files/SpiralBound/studycard.cpp
+++ Qt Project Files/SpiralBound/studycard.cpp
@@ -7,6 +7,10 @@
 extern int side, index;
 extern Deck* copyDeck;
 
+
+/*
+ * Constructor
+*/
 studycard::studycard(QWidget *parent, Deck* deck) :
     QDialog(parent),
     ui(new Ui::studycard)
@@ -15,7 +19,7 @@ studycard::studycard(QWidget *parent, Deck* deck) :
 
     ui->label_deckName->setText(deck->name);
     ui->textBrowser->setText(deck->front[0]);
-    ui->pushButton_previous->hide();
+    ui->pushButton_previous->setEnabled(false);
     copyDeck = deck;
 
     if (index == copyDeck->front.length())
@@ -32,12 +36,18 @@ studycard::studycard(QWidget *parent, Deck* deck) :
     }
 }
 
-
+/*
+ * Destructor
+*/
 studycard::~studycard()
 {
     delete ui;
 }
 
+/*
+ * on_pushButton_flip_clicked() will "flip" the card from front to back,
+ * or from back to front.
+*/
 void studycard::on_pushButton_flip_clicked()
 {
     ui->textBrowser->clear();
@@ -55,44 +65,58 @@ void studycard::on_pushButton_flip_clicked()
     }
 }
 
-
+/*
+ * on_pushButton_ok_clicked() will close the window when the user
+ * clicks "Ok" button. This will usually signify the user being
+ * finished with studying.
+*/
 void studycard::on_pushButton_ok_clicked()
 {
     this->close();
 }
 
+/*
+ * on_pushButton_previous_clicked() will go back one card.
+ * If the deck being studied is at index 0, then the button
+ * will not be available to push.
+*/
 void studycard::on_pushButton_previous_clicked()
 {
     side = 0;
     index--;
     if (index == 0)
     {
-        ui->pushButton_previous->hide();
-        ui->pushButton_next->show();
+        ui->pushButton_next->setEnabled(true);
+        ui->pushButton_previous->setEnabled(false);
     }
     else
     {
-        ui->pushButton_previous->show();
-        ui->pushButton_next->show();
+        ui->pushButton_next->setEnabled(true);
+        ui->pushButton_previous->setEnabled(true);
     }
     ui->textBrowser->clear();
     ui->textBrowser->setText(copyDeck->front[index]);
     qDebug() << "Index: " << index;
 }
 
+/*
+ * on_pushButton_next_clicked() will go to the next card in the
+ * index. If the deck is at the end of the deck, then the button
+ * will not be available to be clicked.
+*/
 void studycard::on_pushButton_next_clicked()
 {
     side = 0;
     index++;
     if (index == copyDeck->front.length() - 1)
     {
-        ui->pushButton_next->hide();
-        ui->pushButton_previous->show();
+        ui->pushButton_next->setEnabled(false);
+        ui->pushButton_previous->setEnabled(true);
     }
     else
     {
-        ui->pushButton_next->show();
-        ui->pushButton_previous->show();
+        ui->pushButton_next->setEnabled(true);
+        ui->pushButton_previous->setEnabled(true);
     }
     ui->textBrowser->clear();
     ui->textBrowser->setText(copyDeck->front[index]);
